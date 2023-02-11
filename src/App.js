7696eedbafa3e758e0fe890aaf66255f0846fff5
Shafayathub <shafayat.ms@gamilcom>
2023-02-11 13:28:08 +0600

    initial set up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button className="btn btn-primary">primary</button>
+      <button className="btn btn-secondary">Success</button>
+      <button className="btn btn-accent">Warning</button>
+      <button className="btn btn-neutral">Error</button>
     </div>
   );
 }
